2ed9923a968cc2f66f9728f9c123a445a663e8c5
JorgePeju <pejuco@gmail.com>
2023-04-19 11:23:00 +0200

    refactor

--- src/hooks/useTodoList.js
+++ src/hooks/useTodoList.js
@@ -1,41 +1,47 @@
 import { useState, useEffect } from "react";
 
-import { todoReducer } from "../../reducers/todoReducer";
+import { todoReducer } from "../reducers/todoReducer";
 
 export const useTodoList = () => {
+
   const [todos, setTodos] = useState([]);
 
   useEffect(() => {
+
     const storedTodos = JSON.parse(localStorage.getItem("todos"));
+
     if (storedTodos) {
+
       setTodos(storedTodos);
+
     }
   }, []);
 
   useEffect(() => {
+
     localStorage.setItem("todos", JSON.stringify(todos));
+
   }, [todos]);
 
   const addTodo = (data) => {
+
     setTodos((prevTodos) =>
+
       todoReducer(prevTodos, {
+
         type: "[TODO] add todo",
-        payload: {
-         
-          task: data.task,
-          description: data.description,
-          done: false,
-          date: new Date(),
-        },
+
+        payload: data,
+        
       })
     );
   };
 
- 
+
 
   return {
     todos,
     addTodo,
-   
+
   };
 };
--- src/reducers/todoReducer.js
+++ src/reducers/todoReducer.js
@@ -3,27 +3,24 @@
 export const todoReducer = (state = [], action) => {
 
     switch (action.type) {
+
         case '[TODO] add todo':
-            return [...state, ...state,
-                {
-                  id: action.payload.id,
-                  todo: action.payload.todo,
-                  description: action.payload.description,
-                  done: false,
-                  date: action.payload.date
-                }];
-           
+
+            return [...state, action.payload]
 
         case '[TODO] delete todo':
+
             console.log(action.payload)
+
             return state.filter(todo => todo.id !== action.payload.id);
-        
 
         case '[TODO] toggle todo':
-            return 
-        
-        default:
-            return state
-    }
 
+            return
+
+        default:
+
+            return state
+            
+    }
 }
