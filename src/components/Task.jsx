dfb3170c6e4d52f638b774fa8488ccc858184ea2
JorgePeju <pejuco@gmail.com>
2023-04-19 17:15:01 +0200

    cambiamos estado del boton

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,32 +1,26 @@
 
 
-export const Task = ({task,description, id, done, handleDeleteTodo, handleToggleTodo}) => {
-      
+export const Task = ({ task, description, id, done, handleDeleteTodo, handleToggleTodo }) => {
+    let boton = ''
+    if (done == true) {
+        boton = 'COMPLETADO'
+    } else {
+        boton = 'PENDIENTE'
+    }
     return (
-
         <>
-
             <div className="task flex self-left">
-                
                 <p>{task}</p>
                 <p>{description}</p>
-
                 <div className="flex self-right">
-
                     <button
-                    onClick={()=>handleToggleTodo(id,done)}
-                    >Pendiente</button>
-
-
+                        onClick={() => handleToggleTodo(id, done)}
+                    >{`${boton}`}</button>
                     <button
-                    onClick={()=>handleDeleteTodo(id)}
+                        onClick={() => handleDeleteTodo(id)}
                     >Eliminar</button>
-
                 </div>
-
             </div>
-
         </>
-
     )
 }
